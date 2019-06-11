9ad7489b98b310578d2f43cc612864add4496b17
lujun <lujun_1991@163.com>
2019-06-11 20:57:29 +0800

    add some p-ext ins

--- opcodes/p_extension_ins.h
+++ opcodes/p_extension_ins.h
@@ -1,1 +1,41 @@
-{"p.add16", 0, {"I", 0}, "d,s,t", MATCH_P_ADD16, MASK_P_ADD16, match_opcode, 0},
+{"p.add8",   0, {"I", 0}, "d,s,t", MATCH_P_ADD8, MASK_P_ADD8, match_opcode, 0},
+{"p.add16",  0, {"I", 0}, "d,s,t", MATCH_P_ADD16, MASK_P_ADD16, match_opcode, 0},
+{"p.add64",  0, {"I", 0}, "d,s,t", MATCH_P_ADD64, MASK_P_ADD64, match_opcode, 0},
+{"p.ave",    0, {"I", 0}, "d,s,t", MATCH_P_AVE, MASK_P_AVE, match_opcode, 0},
+{"p.bitrev", 0, {"I", 0}, "d,s,t", MATCH_P_BITREV, MASK_P_BITREV, match_opcode, 0},
+
+// 3.6 
+// 3.7 
+// 3.8 
+
+// 3.9
+{"p.clrs8",  0, {"I", 0}, "d,s",  MATCH_P_CLRS8,  MASK_P_CLRS8, match_opcode, 0},
+{"p.clrs16", 0, {"I", 0}, "d,s",  MATCH_P_CLRS16, MASK_P_CLRS16, match_opcode, 0},
+{"p.clrs32", 0, {"I", 0}, "d,s",  MATCH_P_CLRS32, MASK_P_CLRS32, match_opcode, 0},
+
+// 3.12
+{"p.clo8",  0, {"I", 0}, "d,s",  MATCH_P_CLO8,  MASK_P_CLO8, match_opcode, 0},
+{"p.clo16", 0, {"I", 0}, "d,s",  MATCH_P_CLO16, MASK_P_CLO16, match_opcode, 0},
+{"p.clo32", 0, {"I", 0}, "d,s",  MATCH_P_CLO32, MASK_P_CLO32, match_opcode, 0},
+
+// 3.15
+{"p.clz8",  0, {"I", 0}, "d,s",  MATCH_P_CLZ8,  MASK_P_CLZ8, match_opcode, 0},
+{"p.clz16", 0, {"I", 0}, "d,s",  MATCH_P_CLZ16, MASK_P_CLZ16, match_opcode, 0},
+{"p.clz32", 0, {"I", 0}, "d,s",  MATCH_P_CLZ32, MASK_P_CLZ32, match_opcode, 0},
+
+// 3.18
+{"p.cmpeq8", 0, {"I", 0}, "d,s,t", MATCH_P_CMPEQ8, MASK_P_CMPEQ8, match_opcode, 0},
+{"p.cmpeq16", 0, {"I", 0}, "d,s,t", MATCH_P_CMPEQ16, MASK_P_CMPEQ16, match_opcode, 0},
+
+// 3.20
+{"p.cras16", 0, {"I", 0}, "d,s,t", MATCH_P_CRAS16, MASK_P_CRAS16, match_opcode, 0},
+{"p.crsa16", 0, {"I", 0}, "d,s,t", MATCH_P_CRSA16, MASK_P_CRSA16, match_opcode, 0},
+
+// 3.22
+
+
+// 3.23
+{"p.kabs8", 0, {"I", 0}, "d,s",  MATCH_P_KABS8, MASK_P_KABS8, match_opcode, 0},
+{"p.kabs16", 0, {"I", 0}, "d,s",  MATCH_P_KABS16, MASK_P_KABS16, match_opcode, 0},
+
+
